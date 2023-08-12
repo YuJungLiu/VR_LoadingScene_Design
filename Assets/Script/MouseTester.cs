41c44fc7256360f0f74936ab6e6a614396d41d65
Aaron Yu-Jung Liu <40709084+miniQQ@users.noreply.github.com>
2023-08-12 11:10:37 +0800

    System

---
+++ Assets/Script/MouseTester.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.VFX;
+
+public class MouseTester : MonoBehaviour
+{
+    public GameObject Cube;
+    public AudioSource sound;
+    public VisualEffect vfx;
+    public string sceneToLoad;
+    public float fadeDuration = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vfx.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StartCoroutine(PressButton());
+        }
+    }
+
+    public IEnumerator PressButton()
+    {
+        Effect();
+        yield return new WaitForSeconds(6);
+        StartCoroutine(SceneLoader());
+    }
+    public void Effect()
+    {
+        sound.Play();
+        vfx.Play();
+        //VFX Fade
+        Debug.Log("Effect!");
+    }
+    public IEnumerator SceneLoader()
+    {
+        StartCoroutine(FadeOutSound());
+        vfx.Stop();
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+    private IEnumerator FadeOutSound()
+    {
+        float startVolume = sound.volume;
+
+        while (sound.volume > 0)
+        {
+            sound.volume -= startVolume * Time.deltaTime / fadeDuration;
+            yield return null;
+        }
+        sound.volume = 0;
+        sound.Stop();
+    }
+}
